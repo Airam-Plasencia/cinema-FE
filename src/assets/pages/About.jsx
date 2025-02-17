ff5a7936e66b659b9f2f376529ed1821f9c95943
Airam Plasencia <airaplas@gmail.com>
2025-02-17 16:58:35 +0000

    Add New Movies

--- src/assets/pages/About.jsx
+++ src/assets/pages/About.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import icon from '../img/25231.png'; 
 import icon2 from '../img/cinema-30.jpg';
+import linkedinIcon from '../img/Linkedin.png';
 
 const About = () => {
   return (
     <div style={{ minHeight: '100vh', textAlign: 'center', padding: '20px' }}>
-      <img src={icon2} alt="Icono de Airam Plasencia" style={{ width: '650px', height: '450px', marginBottom: '40px' }} />
+      <img src={icon2} alt="Icono de Airam Plasencia" style={{ width: '580px', height: '380px',  }} />
       <h2>Creado con la misma ilusión que Toto ama el cine por Airam Plasencia</h2>
       <a href="https://github.com/Airam-Plasencia" target="_blank" rel="noopener noreferrer">
         <img src={icon} alt="Icono de Airam Plasencia" style={{ width: '50px', height: '50px', marginTop: '20px' }} />
+      </a>
+      <a href="https://www.linkedin.com/in/airam-plasencia-mendoza-a71870121/" target="_blank" rel="noopener noreferrer">
+        <img src={linkedinIcon} alt="LinkedIn Logo" style={{ width: '50px', height: '50px', marginTop: '20px', marginLeft: '20px' }} />
       </a>
     </div>
   );
--- src/assets/pages/AddMovies.jsx
+++ src/assets/pages/AddMovies.jsx
@@ -1,7 +1,190 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddMovies = () => {
-  return <div><h1>Add Movies Page</h1></div>;
+  const [movieData, setMovieData] = useState({
+    title: '',
+    year: '',
+    duration: '',
+    director: '',
+    screenplay: '',
+    cast: '',
+    music: '',
+    genre: '',
+    synopsis: '',
+    posterUrl: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setMovieData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setMovieData((prevData) => ({
+        ...prevData,
+        posterUrl: URL.createObjectURL(file) 
+      }));
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Nueva película agregada:', movieData);
+    
+  };
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Agregar Nueva Película</h1>
+      
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Título: </label>
+          <input
+            type="text"
+            name="title"
+            value={movieData.title}
+            onChange={handleChange}
+            required
+            style={{ padding: '8px', width: '300px' }}
+            />
+        </div>
+        
+        <div>
+          <label>Año: </label>
+          <input
+            type="number"
+            name="year"
+            value={movieData.year}
+            onChange={handleChange}
+            required
+            style={{ padding: '3px',  width: '100px' }}
+            />
+        </div>
+        
+        <div>
+          <label>Duración: </label>
+          <input
+            type="text"
+            name="duration"
+            value={movieData.duration}
+            onChange={handleChange}
+            required
+            style={{ padding: '3px',  width: '100px' }}
+            />
+        </div>
+        
+        <div>
+          <label>Director: </label>
+          <input
+            type="text"
+            name="director"
+            value={movieData.director}
+            onChange={handleChange}
+            required
+            style={{ padding: '3px',  width: '200px' }}
+            />
+        </div>
+        
+        <div>
+          <label>Guionista: </label>
+          <input
+            type="text"
+            name="screenplay"
+            value={movieData.screenplay}
+            onChange={handleChange}
+            required
+            style={{ padding: '3px',  width: '200px' }}
+            />
+        </div>
+        
+        <div>
+          <label>Elenco: </label>
+          <input
+            type="text"
+            name="cast"
+            value={movieData.cast}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        
+        <div>
+          <label>Música: </label>
+          <input
+            type="text"
+            name="music"
+            value={movieData.music}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        
+        <div>
+          <label>Género: </label>
+          <input
+            type="text"
+            name="genre"
+            value={movieData.genre}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        
+        <div>
+          <label>Sinopsis: </label>
+          <textarea
+            name="synopsis"
+            value={movieData.synopsis}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        
+        <div>
+          <label>Seleccionar Póster: </label>
+          <input
+            type="file"
+            accept="image/jpeg"
+            onChange={handleFileChange}
+            required
+          />
+        </div>
+
+        
+        {movieData.posterUrl && (
+          <div style={{ marginTop: '10px' }}>
+            <h4>Vista previa del póster:</h4>
+            <img src={movieData.posterUrl} alt="Poster Preview" style={{ width: '200px', height: 'auto' }} />
+          </div>
+        )}
+
+        
+        <button 
+          type="submit"
+          style={{
+            marginTop: '20px',
+            backgroundColor: '#2CAF50',  
+            color: 'white',
+            padding: '10px 20px',
+            border: 'none',
+            borderRadius: '5px',
+            cursor: 'pointer',
+            fontSize: '16px'
+          }}
+        >
+          Agregar Película
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddMovies;
+
