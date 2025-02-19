12ef0c442d469471db3b8da37efc66fee3674dae
Airam Plasencia <airaplas@gmail.com>
2025-02-19 15:43:13 +0000

    Page Error404

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import About from './assets/pages/About';
 import AddMovies from './assets/pages/AddMovies';
 import Curiosity from './assets/pages/Curiosity';
 import MovieDetail from './assets/pages/MovieDetail';
+import Error404 from './assets/pages/Error404';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/edit/:id" element={<EditMovie />} />
         <Route path="/movies" element={<AddMovies />} />
         <Route path="/contact" element={<Curiosity />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
 
       <Footer />
---
+++ src/assets/pages/Error404.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Error404 = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <img 
+        src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzFjOTM0aG1tMHFuc3d0YTZmZDdzM2h6amQzcmJpNm15OWdmcXRvNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1BE01wmrywRgc/giphy.gif" 
+        alt="Error 404" 
+        style={{ width: '600px', height: 'auto' }} 
+      />
+      <h1>¡Ups! Error 404 Page Not Found</h1>
+    </div>
+  );
+};
+
+export default Error404;
